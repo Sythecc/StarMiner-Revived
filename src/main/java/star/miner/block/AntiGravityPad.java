edc27cc36f36f3c02f658d1c6bc81fa8c83a8112
Sythecc <csanders2091@gmail.com>
2020-04-09 10:19:07 -0500

    Update pads
    Hitboxes, place in any direction, if block "under" it breaks it breaks, starting work on more appropriate anti-grav

--- src/main/java/star/miner/block/AntiGravityPad.java
+++ src/main/java/star/miner/block/AntiGravityPad.java
@@ -2,6 +2,10 @@ package star.miner.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 
@@ -32,24 +36,60 @@ import java.util.Random;
 import javax.annotation.Nullable;
 
 public final class AntiGravityPad extends Block {
+	
+	protected static final AxisAlignedBB AntiGrav_UP_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.0625D, 1.0D);
+	protected static final AxisAlignedBB AntiGrav_DOWN_AABB = new AxisAlignedBB(0.0D, 1.0D, 0.0D, 1.0D, 0.9375D, 1.0D);
+	protected static final AxisAlignedBB AntiGrav_NORTH_AABB = new AxisAlignedBB(0.0D, 0.0D, 1.0D, 1.0D, 1.0D, 0.9375D);
+	protected static final AxisAlignedBB AntiGrav_SOUTH_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 0.0625D);
+	protected static final AxisAlignedBB AntiGrav_WEST_AABB = new AxisAlignedBB(1.0D, 0.0D, 0.0D, 0.9375D, 1.0D, 1.0D);
+	protected static final AxisAlignedBB AntiGrav_EAST_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 0.0625D, 1.0D, 1.0D);
 
-	protected static final AxisAlignedBB CARPET_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.0625D, 1.0D);
+	public static final PropertyEnum<EnumFacing> FACING = PropertyDirection.create("facing");
 
 	public AntiGravityPad() {
 		super(Material.CLOTH);
 		setUnlocalizedName(StarMinerMod.MODID + ".block_anti_gravity_pad");
 		setRegistryName("anti_gravity_pad");
 		setCreativeTab(ModTabs.StarMiner);
+		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.UP));
 	}
 
 	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-		return CARPET_AABB;
+		switch (state.getValue(FACING)) {
+		case UP:
+			return AntiGrav_UP_AABB;
+		case DOWN:
+			return AntiGrav_DOWN_AABB;
+		case NORTH:
+			return AntiGrav_NORTH_AABB;
+		case SOUTH:
+			return AntiGrav_SOUTH_AABB;
+		case WEST:
+			return AntiGrav_WEST_AABB;
+		case EAST:
+			return AntiGrav_EAST_AABB;
+		}
+		return null;
 	}
 
 	public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
-		if (entityIn instanceof EntityLivingBase){
-			System.out.println("Applying levitation");
-			((EntityLivingBase) entityIn).addPotionEffect(new PotionEffect(MobEffects.LEVITATION, 200));
+		if (entityIn instanceof EntityLivingBase) {
+			//System.out.println("Applying levitation");
+			//((EntityLivingBase) entityIn).addPotionEffect(new PotionEffect(MobEffects.LEVITATION, 200));
+		}
+		switch (state.getValue(FACING)) {
+		case UP:
+			entityIn.addVelocity(0, 5, 0);
+		case DOWN:
+			entityIn.addVelocity(0, -5, 0);
+		case NORTH:
+			entityIn.addVelocity(5, 0, 0);
+		case SOUTH:
+			entityIn.addVelocity(-5, 0, 0);
+		case WEST:
+			entityIn.addVelocity(0, 0, 5);
+		case EAST:
+			entityIn.addVelocity(0, 0, -5);
 		}
 	}
 
@@ -61,8 +101,22 @@ public final class AntiGravityPad extends Block {
 		return false;
 	}
 
-	public boolean canPlaceBlockAt(World worldIn, BlockPos pos) {
-		return super.canPlaceBlockAt(worldIn, pos) && this.canBlockStay(worldIn, pos);
+	private boolean canBlockStay(World worldIn, BlockPos pos, IBlockState state) {
+		switch (state.getValue(FACING)) {
+		case UP:
+			return !worldIn.isAirBlock(pos.down());
+		case DOWN:
+			return !worldIn.isAirBlock(pos.up());
+		case NORTH:
+			return !worldIn.isAirBlock(pos.south());
+		case SOUTH:
+			return !worldIn.isAirBlock(pos.north());
+		case WEST:
+			return !worldIn.isAirBlock(pos.east());
+		case EAST:
+			return !worldIn.isAirBlock(pos.west());
+		}
+		return !worldIn.isAirBlock(pos.down());
 	}
 
 	public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos) {
@@ -70,7 +124,7 @@ public final class AntiGravityPad extends Block {
 	}
 
 	private boolean checkForDrop(World worldIn, BlockPos pos, IBlockState state) {
-		if (!this.canBlockStay(worldIn, pos)) {
+		if (!this.canBlockStay(worldIn, pos, state)) {
 			this.dropBlockAsItem(worldIn, pos, state, 0);
 			worldIn.setBlockToAir(pos);
 			return false;
@@ -79,8 +133,32 @@ public final class AntiGravityPad extends Block {
 		}
 	}
 
-	private boolean canBlockStay(World worldIn, BlockPos pos) {
-		return !worldIn.isAirBlock(pos.down());
+	/**
+	 * Called by ItemBlocks just before a block is actually set in the world, to
+	 * allow for adjustments to the IBlockstate
+	 */
+	public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY,
+			float hitZ, int meta, EntityLivingBase placer) {
+		return this.getDefaultState().withProperty(FACING, facing);
+	}
+
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, new IProperty[] { FACING });
+	}
+
+	/**
+	 * Convert the BlockState into the correct metadata value
+	 */
+	public int getMetaFromState(IBlockState state) {
+		return ((EnumFacing) state.getValue(FACING)).getIndex();
+	}
+
+	/**
+	 * Convert the given metadata into a BlockState for this Block
+	 */
+	public IBlockState getStateFromMeta(int meta) {
+		EnumFacing enumfacing = EnumFacing.getFront(meta);
+		return this.getDefaultState().withProperty(FACING, enumfacing);
 	}
 
 	@SideOnly(Side.CLIENT)
